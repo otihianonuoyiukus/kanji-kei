682828ca5854fafc3e376eb6d94bd2e647290fcb
kei <johndennisburgos@gmail.com>
2022-10-16 21:41:28 +0800

    Turn some HTML elements from index.html to a React component

---
+++ src/components/HomeCenterText.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function TextHeader() {
+  return (
+    <h1 className="display-4 text-center">Search for a kanji character</h1>
+  );
+}
+
+function TextBody() {
+  return (
+    <p className="lead">
+      6子コヱ前断リツフ梅講ゃゆじ立易えわ秀最ずま会著ぴもンぱ存流12貫ム締能者ト都泡ぼちは合旧ひ。8医てへじ散秋ラへ契性ヌユヤレ靴元テレ組九マシユ季認べいぶつ本禁皇フタオ康辞エヨアヌ遣緒わ爆93加フツ概聞ぞ患産ラだ質6催さむぞお事画ルけだリ者調面量券トだレよ。
+    </p>
+  );
+}
+
+function HomeCenterText() {
+  return (
+    <>
+      <TextHeader />
+      <TextBody />
+    </>
+  );
+}
+
+export default HomeCenterText;
