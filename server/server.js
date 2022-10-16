244fd2944804f1c9b3c590ba905775cef4d07068
kei <johndennisburgos@gmail.com>
2022-10-16 21:54:32 +0800

    Add a new router for kanji information page and fix a mistake with the name of the '/api/v1/kanji/:kanji' router

--- server/server.js
+++ server/server.js
@@ -10,35 +10,32 @@ app.use("/src/assets/svg/", express.static("src/assets/svg/"));
 app.use("/public/", express.static("public/"));
 app.use("/src/components", assetsRouter);
 
-
 app.get("/api/v1/language-data/kanji_data", (req, res) => {
-	getKanjiList().then(result => res.json(result));
+  getKanjiList().then((result) => res.json(result));
 });
 
 app.get("/api/v1/language-data/radical_data", (req, res) => {
-	getRadicalList().then(result => res.json(result));
+  getRadicalList().then((result) => res.json(result));
 });
 
 //TODO: Handle error if kanji does not exist in the list
-app.get("api/v1/kanji/:kanji", (req, res) => {
-	getKanjiInfo(req.params.kanji).then(result => res.json(result));
+app.get("/api/v1/kanji/:kanji", (req, res) => {
+  getKanjiInfo(req.params.kanji).then((result) => res.json(result));
 });
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-//TODO: This
-// app.get("/kanji/:kanji", (req, res) => {
-
-// });
-
+app.get("/kanji/:kanji", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "kanji-info.html"));
+});
 
 const { PORT = 5000 } = process.env;
 
 app.listen(PORT, () => {
-	console.log();
-	console.log(`  App running in port ${PORT}`);
-	console.log();
-	console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+  console.log();
+  console.log(`  App running in port ${PORT}`);
+  console.log();
+  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
 });
